891df9f33826ef30c1aaf9f2ed86ade2a17d1d78
Missy Williams <missywilliams85@gmail.com>
2015-11-06 15:05:53 -0600

    Get the expandy to work

--- app/destination/destination.controller.js
+++ app/destination/destination.controller.js
@@ -27,6 +27,21 @@
             });
         };
 
+        $scope.collapsibleElements = [{
+        icon: 'mdi-image-filter-drama',
+        title: 'First',
+        content: 'Lorem ipsum dolor sit amet.'
+    },{
+        icon: 'mdi-maps-place',
+        title: 'Second',
+        content: 'Lorem ipsum dolor sit amet.'
+    },{
+        icon: 'mdi-social-whatshot',
+        title: 'Third',
+        content: 'Lorem ipsum dolor sit amet.'
+    }
+];
+
         $scope.getIslandsByDestination = function(dest){
           var destIslands = {};
           for(var i = 0; i < islands.length; i++){
